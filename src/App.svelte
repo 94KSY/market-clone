77f5dd00400736610886856a6b2ed022ade907d7
94KSY <gin4047@naver.com>
2024-03-14 21:53:48 +0900

    svelte, vite로 바꾼 후 firebase deploy 세팅

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  // 모듈 방식 : 각각 나눠서 작업하고 가져와서 쓰는 방식
+  import Main from "./pages/Main.svelte";
+  import Login from "./pages/Login.svelte";
+  import Signup from "./pages/Signup.svelte";
+  import Write from "./pages/Write.svelte";
+  import Router from "svelte-spa-router";
+  import NotFound from "./pages/NotFound.svelte";
+  import "./css/style.css";
+
+  const routes = {
+    "/": Main,
+    "/login": Login,
+    "/signup": Signup,
+    "/write": Write,
+    "*": NotFound,
+  };
+</script>
+
+<Router {routes} />
