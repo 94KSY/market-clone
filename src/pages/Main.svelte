5428614b21320ab9beca691f9e50e61f49e47317
94KSY <gin4047@naver.com>
2024-03-15 15:41:30 +0900

    유저 인증까지 파이어베이스로 구현

--- src/pages/Main.svelte
+++ src/pages/Main.svelte
@@ -1,9 +1,36 @@
 <script>
+  import { onMount } from "svelte";
+  import { getDatabase, ref, onValue } from "firebase/database";
+  import Nav from "../components/Nav.svelte";
+
   let hour = new Date().getHours();
   let min = new Date().getMinutes();
-  let time = "20:12";
 
-  setInterval(() => (min = min + 1), 1000);
+  $: items = []; // let: items대신 쓰면 items라는 변수는 반응형 변수로 됨 따라서 값이 바뀌면 자동 업뎃
+
+  const calcTime = (timestamp) => {
+    const curTime = new Date().getTime() - 9 * 60 * 60 * 1000;
+    const time = new Date(curTime - timestamp);
+    const hour = time.getHours();
+    const minute = time.getMinutes();
+    const second = time.getSeconds();
+
+    if (hour > 0) return `${hour}시간 전`;
+    else if (minute > 0) return `${minute}분 전`;
+    else if (second > 0) return `${second}초 전`;
+    else return "방금 전";
+  };
+
+  const db = getDatabase();
+  const itemsRef = ref(db, "items/");
+
+  onMount(() => {
+    // onMount -> 화면이 렌더링 됐을 때 함수가 실행되도록 하는 명령어 ex)글쓰기 갔다 홈으로와도 데이터가 그대로
+    onValue(itemsRef, (snapshot) => {
+      const data = snapshot.val();
+      items = Object.values(data);
+    });
+  });
 </script>
 
 <header>
@@ -31,42 +58,25 @@
 </header>
 
 <main>
-  <a class="write-btn" href="#/write.html">+ 글쓰기</a>
+  {#each items as item}
+    <div class="item-list">
+      <div class="item-list__img">
+        <img alt={item.title} src={item.imgUrl} />
+      </div>
+      <div class="item-list__info">
+        <div class="item-list__info-title">{item.title}</div>
+        <div class="item-list__info-price">{item.price}</div>
+        <div>{item.description}</div>
+        <div class="item-list__info-meta">
+          {item.place}
+          {calcTime(item.insertAt)}
+        </div>
+      </div>
+    </div>
+  {/each}
+  <a class="write-btn" href="#/write">+ 글쓰기</a>
 </main>
 
-<footer>
-  <div class="footer-block">
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/home.svg" alt="" />
-      </div>
-      <div class="footer-icons__desc">홈</div>
-    </div>
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/doc.svg" alt="" />
-      </div>
-      <div class="footer-icons__desc">동네생활</div>
-    </div>
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/location.svg" alt="" />
-      </div>
-      <div class="footer-icons__desc">내 근처</div>
-    </div>
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/chat.svg" alt="" />
-      </div>
-      <div class="footer-icons__desc">채팅</div>
-    </div>
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/user.svg" alt="" />
-      </div>
-      <div class="footer-icons__desc">나의 당근</div>
-    </div>
-  </div>
-</footer>
+<Nav location="home" />
 
 <div class="media-info-msg">화면 사이즈를 줄여주세요.</div>
--- src/pages/Write.svelte
+++ src/pages/Write.svelte
@@ -1,25 +1,93 @@
-<form id="write-form">
+<script>
+  import { getDatabase, ref, push } from "firebase/database";
+  import {
+    getStorage,
+    ref as refImage,
+    uploadBytes,
+    getDownloadURL,
+  } from "firebase/storage";
+  import Nav from "../components/Nav.svelte";
+
+  let title;
+  let price;
+  let description;
+  let place;
+  let files;
+
+  const storage = getStorage();
+  const db = getDatabase();
+
+  function writeUserData(imgUrl) {
+    push(ref(db, "items/"), {
+      // set로하면 같은 데이터가 들어가면 그 같은값을 업데이트해서 push로 새로운 글로 들어가게
+      title,
+      price,
+      description,
+      place,
+      insertAt: new Date().getTime(),
+      imgUrl,
+    });
+    alert("글쓰기 완료");
+    window.location.hash = "/";
+  }
+
+  const uploadFile = async () => {
+    const file = files[0];
+    const name = file.name;
+    const imgRef = refImage(storage, name);
+    await uploadBytes(imgRef, file);
+    const url = await getDownloadURL(imgRef);
+    return url;
+  };
+
+  const handleSubmit = async () => {
+    const url = await uploadFile();
+    writeUserData(url);
+  };
+</script>
+
+<form id="write-form" on:submit|preventDefault={handleSubmit}>
+  <!-- preventDefault를 여기서 해줌 -->
   <div>
     <label for="image">이미지</label>
-    <input type="file" id="image" name="image" />
+    <input type="file" bind:files id="image" name="image" />
   </div>
   <div>
     <label for="title">제목</label>
-    <input type="text" id="title" name="title" />
+    <input type="text" id="title" name="title" bind:value={title} />
+    <!-- bind는 위에 title과 연동시킬 때 씀 -->
   </div>
   <div>
     <label for="price">가격</label>
-    <input type="number" id="price" name="price" />
+    <input type="number" id="price" name="price" bind:value={price} />
   </div>
   <div>
     <label for="description">설명</label>
-    <input type="text" id="description" name="description" />
+    <input
+      type="text"
+      id="description"
+      name="description"
+      bind:value={description}
+    />
   </div>
   <div>
     <label for="place">장소</label>
-    <input type="text" id="place" name="place" />
+    <input type="text" id="place" name="place" bind:value={place} />
   </div>
   <div>
-    <button type="submit">글쓰기 완료!</button>
+    <button class="write-button" type="submit">글쓰기 완료!</button>
   </div>
 </form>
+
+<Nav location="write" />
+
+<style>
+  .write-button {
+    background-color: black;
+    margin: 10px;
+    border-radius: 10px;
+    padding: 5px 12px 5px 12px;
+    color: white;
+    cursor: pointer;
+  }
+</style>
